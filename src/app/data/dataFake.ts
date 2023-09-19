20cdfb53b8cb497c24e98711795513b2dba76d91
MarlonDougs <marlondouglas2@hotmail.com>
2023-09-19 18:26:04 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://m.media-amazon.com/images/I/418Vz+WLU6L.jpg",
+    "title": "The Beginning After the End  Volume 01",
+    "description": "O rei Gray é um governante notável com um reinado próspero, mas, no entanto… a opulência e o sucesso não podem enterrar o vazio e a solidão. Seu tempo como rei, contudo, chega a um fim abrupto e ele reencarna como Arthur Leywin – um bebê nascido em um mundo de magia e monstros. Equipado com memórias de sua vida anterior e o desejo de proteger o calor recém-descoberto que o cerca, Art começa sua jornada para se tornar um poderoso guerreiro mais uma vez."
+  },
+  {
+    "id":"2",
+    "photoCover":"https://m.media-amazon.com/images/I/81QgeYzlX3L._AC_UF1000,1000_QL80_.jpg",
+    "title": "Saiu novo filme",
+    "description": "ola"
+  }
+]
