5a9adf39e3cb0171fc5d00193cade57c4bc54a15
MarlonDougs <marlondouglas2@hotmail.com>
2023-09-19 19:35:24 -0300

    small CSS tweaks

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
   {
     "id":"1",
     "photoCover":"https://m.media-amazon.com/images/I/418Vz+WLU6L.jpg",
-    "title": "The Beginning After the End  Volume 01",
-    "description": "O rei Gray é um governante notável com um reinado próspero, mas, no entanto… a opulência e o sucesso não podem enterrar o vazio e a solidão. Seu tempo como rei, contudo, chega a um fim abrupto e ele reencarna como Arthur Leywin – um bebê nascido em um mundo de magia e monstros. Equipado com memórias de sua vida anterior e o desejo de proteger o calor recém-descoberto que o cerca, Art começa sua jornada para se tornar um poderoso guerreiro mais uma vez."
+    "title": "The Beginning After the End  Vol. 1",
+    "description": "I never believed in the whole “light at the end of the tunnel” folly where people, after having a near-death experience, would startle awake in a cold sweat exclaiming, “I saw the light!” But there I was, in this so-called “tunnel” facing a glaring light, when the last thing I remembered was sleeping in my room—the royal bedchamber, as others called it. Had I died? If so, how?King Grey once benefited from unrivaled strength, wealth, and prestige in a world governed by martial ability, but now finds himself reborn in a society dominated by magic. Given a second chance at life, the once-king strives to understand his role in this new world and the purpose of his reincarnation while correcting the mistakes of his past."
   },
   {
     "id":"2",
     "photoCover":"https://m.media-amazon.com/images/I/81QgeYzlX3L._AC_UF1000,1000_QL80_.jpg",
-    "title": "Saiu novo filme",
-    "description": "ola"
-  }
+    "title": "The Beginning After the End, Vol. 2",
+    "description": "Reincarnated king-turned-kid Arthur Leywin and his travel companions have been ambushed by bandits, and Art only has one thing on his mind: protect his family. But he soon realizes his strong mana core alone might not be enough to get everyone out of danger―even with the knowledge and experience from his old life, this new world is a formidable place. And when mythical creatures and slave traders are thrown into the mix, Art’s going to have to pull out all the stops if he wants to stand his ground!"
+  },
+  {
+    "id":"3",
+    "photoCover":"https://m.media-amazon.com/images/I/81W5HtwwKTL._AC_UF1000,1000_QL80_.jpg",
+    "title": "The Beginning After the End  Vol. 3",
+    "description": "Was I taking this life for granted? Having this amount of power at my age had made me lose some of the sense of rationality I’d had in my former life. No longer a king, no longer tied down by rules and politics, I’d become careless. In this world, my potential was boundless.Though Arthur Leywin would like nothing more than to pursue his studies at the esteemed Xyrus Academy, where he hopes to push his already potent magical abilities to new plateaus and perhaps discover something more about his reincarnation into this world, he finds the politics of the school’s halls even more challenging to navigate than those of the council chambers he once inhabited as King Grey. Arthur faces inept professors, jealous classmates, and an overeager director while fending off the affection of an old friend and the aggression of an old enemy. Then a routine class trip into a low-level dungeon turns out to be anything but, and Arthur discovers something that will change his life forever."
+  },
+  {
+    "id":"4",
+    "photoCover":"https://m.media-amazon.com/images/I/41y+rXwZraL.jpg",
+    "title": "The Beginning After the End  Vol. 4",
+    "description": "After years in the elven kingdom, Arthur is finally able to travel to the city of Xyrus...and reunite with his family! In Leywin fashion, the reunion can’t be celebrated without a friendly battle between father and son, but both Rey and Art have come a long way since their training days. When the dust settles, who will come out on top? And what’s this about Art enrolling in Xyrus Academy?!"
+  },
 ]
